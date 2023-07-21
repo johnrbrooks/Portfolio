23f882a0e03a010a779a90e808e9ce150debb205
John Brooks <johnrbrooks93@gmail.com>
2023-07-21 17:18:27 -0400

    Refactored images

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,15 @@
+import mail from '../assets/images/mail.png'
+import linkedIn from '../assets/images/linkedin-logo.png'
+import github from '../assets/images/github-sign.png'
 import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return(
         <div className="footer-bar">
             <div className='footer-links'>
-                <a href="" className='footer-image-links2'><img src="src/assets/images/mail.png" alt="contact" className='footer-image2'/></a>
-                <a href="" className='footer-image-links'><img src="src/assets/images/github-sign.png" alt="github" className='footer-image'/></a>
-                <a href="" className='footer-image-links'><img src="src/assets/images/linkedin-logo.png" alt="linkedin" className='footer-image'/></a>
+                <a href="" className='footer-image-links2'><img src={mail} alt="contact" className='footer-image2'/></a>
+                <a href="" className='footer-image-links'><img src={linkedIn} alt="github" className='footer-image'/></a>
+                <a href="" className='footer-image-links'><img src={github} alt="linkedin" className='footer-image'/></a>
             </div>
             <p className="footer-credits">Designed & built by John Brooks</p>
         </div>
