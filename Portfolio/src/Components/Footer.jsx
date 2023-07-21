94d5d1b02e40ed6002e76adfc401c7ae528588d0
John Brooks <johnrbrooks93@gmail.com>
2023-07-20 23:16:32 -0400

    Footer started

--- Portfolio/src/Components/Footer.jsx
+++ Portfolio/src/Components/Footer.jsx
@@ -3,7 +3,12 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
     return(
         <div className="footer-bar">
-            <h1>This is the footer</h1>
+            <div className='footer-links'>
+                <a href="" className='footer-image-links2'><img src="src/assets/images/mail.png" alt="contact" className='footer-image2'/></a>
+                <a href="" className='footer-image-links'><img src="src/assets/images/github-sign.png" alt="github" className='footer-image'/></a>
+                <a href="" className='footer-image-links'><img src="src/assets/images/linkedin-logo.png" alt="linkedin" className='footer-image'/></a>
+            </div>
+            <p className="footer-credits">Designed & built by John Brooks</p>
         </div>
     )
 }
